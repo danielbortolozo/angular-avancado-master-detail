5ab6a4bd302a7a755785246b76e2f24ba3421c64
Daniel Bortolozo <danielbortolozo@hotmail.com>
2019-09-22 20:12:17 -0300

    adding in-memory-database for categories

---
+++ src/app/in-memory-databes.ts
@@ -0,0 +1,14 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+
+export class InMemoryDatabase implements InMemoryDbService{
+    createDb(){
+        const categories = [
+            { id: 1, name: "Moradia", descripition: "Pagamento de conta de casa"},
+            { id: 2, name: "Saúde", descripition: "Plano de Saúde e Remédios"},
+            { id: 3, name: "Lazer", descripition: "Cinema, parque, clube, restaurantes etc."},
+            { id: 4, name: "Salário", descripition: "Recebimento de Salário"},
+            { id: 5, name: "Freelas", descripition: "Trabalhos como freelancer"}          
+        ];
+        return { categories }
+    }
+}
